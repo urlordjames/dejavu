b1bd27f62d608b02b0aaa264a68e5a4b39a33313
Sol <solsahn@gmail.com>
2021-07-24 16:45:04 -0400

    player and projectiles move as one

---
+++ game/script.js
@@ -0,0 +1,56 @@
+let canvas = document.getElementById('pengis');
+let ctx = canvas.getContext('2d');
+canvas.style.background = "#222";
+
+class Player {
+    constructor(position, radius, color) {
+        this.position = position;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, 2*Math.PI);
+        ctx.fill();
+    }
+}
+
+class Projectile {
+    constructor(position, radius, color, velocity) {
+        this.position = position;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, 2*Math.PI);
+        ctx.fill();
+    }
+    update() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        this.draw();
+    }
+}
+
+const player = new Player({x: canvas.width/2, y: canvas.height/2}, 12, "red");
+const projectiles = [];
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    projectiles.forEach((projectile) => {
+        projectile.update();
+    })
+    requestAnimationFrame(animate);
+}
+
+canvas.addEventListener("click", (event) => {
+    const angle = Math.atan2(event.clientY - player.position.y, event.clientX - player.position.x);
+    const velocity = {x: Math.cos(angle), y: Math.sin(angle)};
+    projectiles.push(new Projectile(player.position, 6, "blue", velocity));
+});
+
+animate();
